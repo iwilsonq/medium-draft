f02355861f348d3dba0338206eb75304dce4bcbc
Ian Wilson <iwilsonq@gmail.com>
2018-05-25 01:28:19 -0700

    add lib

--- src/components/blocks/image.js
+++ src/components/blocks/image.js
@@ -1,59 +1,59 @@
-import PropTypes from 'prop-types'
-import React from 'react'
+import PropTypes from 'prop-types';
+import React from 'react';
 
-import { EditorBlock, EditorState, SelectionState } from 'draft-js'
+import { EditorBlock, EditorState, SelectionState } from 'draft-js';
 
-import { getCurrentBlock } from '../../model/'
+import { getCurrentBlock } from '../../model/';
 
 class ImageBlock extends React.Component {
-	static propTypes = {
-		block: PropTypes.object,
-		blockProps: PropTypes.object,
-		includeCaption: PropTypes.bool
-	}
+  static propTypes = {
+    block: PropTypes.object,
+    blockProps: PropTypes.object,
+    includeCaption: PropTypes.bool,
+  }
 
-	static defaultProps = {
-		includeCaption: false
-	}
+  static defaultProps = {
+    includeCaption: false,
+  }
 
-	focusBlock = () => {
-		const { block, blockProps } = this.props
-		const { getEditorState, setEditorState } = blockProps
-		const key = block.getKey()
-		const editorState = getEditorState()
-		const currentblock = getCurrentBlock(editorState)
-		if (currentblock.getKey() === key) {
-			return
-		}
-		const newSelection = new SelectionState({
-			anchorKey: key,
-			focusKey: key,
-			anchorOffset: 0,
-			focusOffset: 0
-		})
-		setEditorState(EditorState.forceSelection(editorState, newSelection))
-	}
+  focusBlock = () => {
+    const { block, blockProps } = this.props;
+    const { getEditorState, setEditorState } = blockProps;
+    const key = block.getKey();
+    const editorState = getEditorState();
+    const currentblock = getCurrentBlock(editorState);
+    if (currentblock.getKey() === key) {
+      return;
+    }
+    const newSelection = new SelectionState({
+      anchorKey: key,
+      focusKey: key,
+      anchorOffset: 0,
+      focusOffset: 0,
+    });
+    setEditorState(EditorState.forceSelection(editorState, newSelection));
+  }
 
-	render() {
-		const { block, includeCaption } = this.props
-		const data = block.getData()
-		const src = data.get('src')
-		if (src !== null) {
-			return (
-				<div>
-					<div className="md-block-image-inner-container" onClick={this.focusBlock}>
-						<img role="presentation" src={src} />
-					</div>
-					{includeCaption && (
-						<figcaption>
-							<EditorBlock {...this.props} />
-						</figcaption>
+  render() {
+    const { block, includeCaption } = this.props;
+    const data = block.getData();
+    const src = data.get('src');
+    if (src !== null) {
+      return (
+        <div>
+          <div className="md-block-image-inner-container" onClick={this.focusBlock}>
+            <img role="presentation" src={src} />
+          </div>
+          {includeCaption && (
+          <figcaption>
+            <EditorBlock {...this.props} />
+          </figcaption>
 					)}
-				</div>
-			)
-		}
-		return <EditorBlock {...this.props} />
-	}
+        </div>
+      );
+    }
+    return <EditorBlock {...this.props} />;
+  }
 }
 
-export default ImageBlock
+export default ImageBlock;
